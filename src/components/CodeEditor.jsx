b6b14ed3f22309edfdd0af8d68dcd1fe41c44e38
shambolic-samreeeed <samreedmaharjan1899@gmail.com>
2024-09-18 13:05:19 +0545

    final changes for today

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import Editor from '@monaco-editor/react';
+import { useState } from 'react';
 
 const CodeEditor = () => {
+
+    const [value, setValue] = useState()
+
   return (
     <div className='dark:bg-black'>
       <Editor
-        height='90vh'
+        height='50vh'
         defaultLanguage='javascript'
         defaultValue='// your code here'
         options={{ minimap: { enabled: false } }}
         theme='vs-dark'
+        value = {value}
+        onChange={
+            (value)=>setValue(value)
+        }
+
         onMount={(editor, monaco) => {
           monaco.editor.defineTheme('transparentTheme', {
             base: 'vs-dark',
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div>
         <Header/>
-        <CodeEditor />
+        <CodeEditor/>
     </div>
   )
 }
